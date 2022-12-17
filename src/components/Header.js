891fcf6f8a206570042ef99755c6e61565360223
chemokita13 <chemokita13@gmail.com>
2022-12-17 20:54:59 +0100

    header checkpoint

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import "./header.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <h1 className="titles" id="name">
+                Jos&eacute; Mar&iacute;a Pahino
+            </h1>
+            <h2 className="titles" id="work">
+                Full-Stack developer
+            </h2>
+        </header>
+    );
+}
+
+export default Header;
