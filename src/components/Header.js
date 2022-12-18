98c67bfe0e1611269d22f3a2cf5afd4364da744f
chemokita13 <chemokita13@gmail.com>
2022-12-18 19:49:13 +0100

    header animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,107 +1,29 @@
 import "./header.css";
-import { useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import Pulse from "react-reveal/Pulse";
+import Jump from "react-reveal/Jump";
 
 function Header() {
-    const particlesInit = useCallback(async (engine) => {
-        await loadFull(engine);
-    }, []);
-
     return (
         <div className="header component">
-            <Particles
-                id="tsparticles"
-                init={particlesInit}
-                options={{
-                    fullScreen: false,
-                    background: {
-                        color: {
-                            value: "#000",
-                        },
-                    },
-                    fpsLimit: 120,
-                    interactivity: {
-                        events: {
-                            onClick: {
-                                enable: true,
-                                mode: "push",
-                            },
-                            onHover: {
-                                enable: false,
-                                mode: "push",
-                            },
-                            resize: true,
-                        },
-                        modes: {
-                            push: {
-                                quantity: 4,
-                            },
-                            repulse: {
-                                distance: 200,
-                                duration: 0.4,
-                            },
-                        },
-                    },
-                    particles: {
-                        color: {
-                            value: "#ffffff",
-                        },
-                        links: {
-                            color: "#ffffff",
-                            distance: 150,
-                            enable: true,
-                            opacity: 0.5,
-                            width: 1,
-                        },
-                        collisions: {
-                            enable: true,
-                        },
-                        move: {
-                            directions: "none",
-                            enable: true,
-                            outModes: {
-                                default: "bounce",
-                            },
-                            random: false,
-                            speed: 2,
-                            straight: false,
-                        },
-                        number: {
-                            density: {
-                                enable: true,
-                                area: 800,
-                            },
-                            value: 80,
-                        },
-                        opacity: {
-                            value: 0.5,
-                        },
-                        shape: {
-                            type: "triangle",
-                        },
-                        size: {
-                            value: { min: 1, max: 5 },
-                        },
-                    },
-                    detectRetina: true,
-                }}
-            />
-            <div className="titles-container">
-                <h1 className="titles" id="name">
-                    Jos&eacute; Mar&iacute;a Pahino
-                </h1>
-                <h2 className="titles" id="work">
-                    Junior full stack developer
-                </h2>
-            </div>
-            <a href="#pseudo-body">
-                <div className="scroll-btn">
-                    <span></span>
-                    <span></span>
-                    <span></span>
+            <Pulse>
+                <div className="titles-container">
+                    <h1 className="titles" id="name">
+                        Jos&eacute; Mar&iacute;a Pahino
+                    </h1>
+                    <h2 className="titles" id="work">
+                        Junior full stack developer
+                    </h2>
                 </div>
-            </a>
+            </Pulse>
+            <Jump forever={true}>
+                <a href="#about">
+                    <div className="scroll-btn">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </a>
+            </Jump>
         </div>
     );
 }
