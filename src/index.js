4a4d3e81e3356ee63becb355bf231a0773f4888b
chemokita13 <chemokita13@gmail.com>
2022-12-25 14:41:28 +0100

    vercel analytics

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Analytics } from "@vercel/analytics/react"; // vercel analytics to view what people are searching hahahah
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.Fragment>
         <App />
+        <Analytics />
     </React.Fragment>
 );
 
